10ce7a42272bc54aff241ac23573df7901d38e6c
228184520lyd-boop <228184520lyd@gmail.com>
2026-08-01 22:15:55 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/** Mirror Abyss mobile-safe floating loader 2.0.0-lite.ui.46-progress-recall. */
+/** Mirror Abyss mobile-safe floating loader 2.0.0-lite.ui.47-worldbook-source. */
 const LOADER_ID = 'mirror-abyss-loader-control';
 const APP_ROOT_ID = 'mirror-abyss-core-control';
 let loaded;
