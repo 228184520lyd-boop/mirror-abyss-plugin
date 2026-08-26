cc6a2bb8624adf6b9b468bd5dc38fefba6e3086c
228184520lyd-boop <228184520lyd@gmail.com>
2026-08-27 00:15:13 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { createApplication } from './app.js?v=4.0.32-recovered.2';
+import { createApplication } from './app.js?v=4.0.32-recovered.3';
 
 export function createLifecycleHooks(applicationFactory = createApplication, logger = console) {
   let application;
