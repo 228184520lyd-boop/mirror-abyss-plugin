380157cfc4c7212b4afc094d92aac364f3fd86a0
228184520lyd-boop <228184520lyd@gmail.com>
2026-08-29 00:02:12 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-import { startApplication } from './app.js?v=4.0.54';
-
-const getContext = globalThis.SillyTavern?.getContext;
-if (typeof getContext !== 'function') throw new Error('SillyTavern 尚未就绪');
-const initialContext = getContext();
-
-startApplication(initialContext, handleHostEvent => {
-  const names = [
-    'MESSAGE_RECEIVED', 'MESSAGE_EDITED', 'MESSAGE_SWIPED', 'MESSAGE_DELETED',
-    'CHAT_CHANGED', 'GENERATION_STARTED', 'WORLD_INFO_ACTIVATED', 'WORLDINFO_UPDATED',
-  ];
-  for (const name of names) {
-    const event = initialContext.eventTypes?.[name];
-    if (event) initialContext.eventSource?.on?.(event, (...args) => handleHostEvent(name, ...args));
-  }
-});
+import { startApplication } from './app.js?v=4.0.63';
+const ctx = globalThis.SillyTavern?.getContext?.();
+if (ctx) startApplication(ctx);
