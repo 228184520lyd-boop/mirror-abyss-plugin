ed61f89cfd5cc2bf45bdede04d2b587583edcabc
228184520lyd-boop <228184520lyd@gmail.com>
2026-09-01 20:14:28 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-import { startApplication } from './app.js?v=4.0.118';
+import { startApplication } from './app.js?v=4.0.122';
 const ctx = globalThis.SillyTavern?.getContext?.();
 if (ctx) startApplication(ctx);
